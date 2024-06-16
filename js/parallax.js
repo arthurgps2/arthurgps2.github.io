f64a9099bc6280254f5db9d611d60a52c71c4a0a
arthurgps2 <72627895+arthurgps2@users.noreply.github.com>
2024-06-16 14:27:35 -0300

    Added parallax to footer

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,13 @@
 document.addEventListener("scroll", () => {
+    // Header parallax
     let header = document.querySelector("header img");
-    let value = Math.min(window.scrollY / header.offsetHeight, 1);
-    header.style.transform = `translateY(${value * 75}%)`;
+    let value1 = Math.min(window.scrollY / header.offsetHeight, 1);
+    header.style.transform = `translateY(${value1 * 75}%)`;
+
+    // Footer parallax
+    let footer = document.querySelector("footer");
+    let rect = footer.getBoundingClientRect();
+    let value2 = Math.min((Math.round(rect.bottom) - window.innerHeight) / rect.height, 1);
+    let footerBg = document.querySelector(".footer-bg");
+    footerBg.style.transform = `translateY(${value2 * 20}%)`;
 });
